e74713c0a2c059f1fbe6b234db27769a889c7ebd
Jacob Lepler <lepler.jacob@gmail.com>
2021-02-25 16:00:14 -0500

    challenge one done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,37 @@
 // Base URL
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = "https://dog.ceo/api/breeds/list/all"
+
+document.addEventListener('DOMContentLoaded', () => {
+    addImgElement();
+    addBreeds();
+})
+
+// Another function
 
 // Upon page load fetch images from api
-document.addEventListener('DOMContentLoaded', () => {
+const addImgElement = () => {
+    // Grab image container html
+    const imgContainer = document.querySelector('#dog-image-container');
     
-})
+    // Creates ul element
+    let ul = document.createElement('ul');
+    ul.className = "img-list"
+    ul.style.listStyleType = "none";
+    imgContainer.appendChild(ul)
+    
+    // fetch images from api
+    fetch(imgUrl).then(res => res.json()).then(data => data.message.forEach((img) => {
+        // create li and adds image
+        let li = document.createElement('li');
+        let imgEl = document.createElement('img')
+        imgEl.src = img
+        imgEl.style.width = "400px"
+        li.append(imgEl)
+        ul.appendChild(li)
+    }))
+}
+
+const addBreeds = () => {
+    
+}
